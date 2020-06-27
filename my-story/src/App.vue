b59f118935c37e0bdeed01112060c62b9dbf7c3d
lenghuang <lendevelops@gmail.com>
2020-06-27 18:37:59 -0400

    basic scroll effect

--- my-story/src/App.vue
+++ my-story/src/App.vue
@@ -1,16 +1,39 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <div id='app'>
+    <NavBar/>
     <HelloWorld/>
+    <HelloWorld/>
+    <HelloWorld/>
+    <HelloWorld/>
+    <div id='who'> scroll here </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import NavBar from './components/NavBar'
+import Vue from 'vue'
+import VueScrollTo from 'vue-scrollto'
+
+// You can also pass in the default options
+Vue.use(VueScrollTo, {
+  container: 'body',
+  duration: 1000,
+  easing: 'ease-out',
+  offset: 0,
+  force: true,
+  cancelable: true,
+  onStart: false,
+  onDone: false,
+  onCancel: false,
+  x: false,
+  y: true
+})
 
 export default {
   name: 'App',
   components: {
+    NavBar,
     HelloWorld
   }
 }
@@ -21,8 +44,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #22223b;
 }
 </style>
---
+++ my-story/src/components/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="navbar-fixed">
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#" style="padding-left: 3rem;" class="brand-logo">Len Huang</a>
+        <ul id="nav-mobile" style="padding-right: 3rem;" class="right hide-on-med-and-down">
+          <li><a href="#" v-scroll-to="'#who'">Who</a></li>
+          <li><a href="#">What</a></li>
+          <li><a href="#">When</a></li>
+          <li><a href="#">Where</a></li>
+          <li><a href="#">Why</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+nav {
+  background-color: #c9ada7;
+}
+a {
+  color: #4a4e69;
+}
+</style>
